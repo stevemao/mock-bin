7cd05772d90f04e75c2f28e15aae660ca645650e
Steve Mao <maochenyan@gmail.com>
2017-05-14 09:44:36 +1000

    update deps, drop unsupported node versions

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
 'use strict';
-var shell = require('shelljs');
-var tempWrite = require('temp-write');
-var shebangRegex = require('shebang-regex');
-var cmdShim = require('cmd-shim');
-var Promise = require('pinkie-promise');
-var pify = require('pify');
+const shell = require('shelljs');
+const tempWrite = require('temp-write');
+const shebangRegex = require('shebang-regex');
+const cmdShim = require('cmd-shim');
+const Promise = require('pinkie-promise');
+const pify = require('pify');
 
-module.exports = function (bin, shebang, code) {
+module.exports = (bin, shebang, code) => {
 	if (!shebangRegex.test(shebang)) {
 		shebang = '#!/usr/bin/env ' + shebang;
 	}
 
 	// On windows shebangs aren't supported. Use a fake extension to prevent
 	// the bin from being executed instead of the shim.
-	var fileName = bin + (process.platform === 'win32' ? '.x-mock-bin' : '');
+	const fileName = bin + (process.platform === 'win32' ? '.x-mock-bin' : '');
 
-	var oldPath = shell.env.PATH;
+	let oldPath = shell.env.PATH;
 
-	var ret = tempWrite(shebang + '\n' + code, fileName)
-		.then(function (filepath) {
+	return tempWrite(shebang + '\n' + code, fileName)
+		.then(filepath => {
 			// Path separator according to platform
-			var sep = process.platform === 'win32' ? ';' : ':';
+			const sep = process.platform === 'win32' ? ';' : ':';
 			shell.chmod('+x', filepath);
 			shell.env.PATH = filepath.replace(new RegExp(fileName + '$'), '') + sep + oldPath;
 
 			if (process.platform === 'win32') {
-				// on windows shebangs aren't supported, add cmd shim
+				// On windows shebangs aren't supported, add cmd shim
 				return pify(cmdShim, Promise)(filepath, filepath.replace(new RegExp('\\.x-mock-bin$'), ''));
 			}
 		})
-		.then(function () {
-			return function () {
-				if (oldPath) {
-					shell.env.PATH = oldPath;
-					oldPath = null;
-				}
-			};
+		.then(() => () => {
+			if (oldPath) {
+				shell.env.PATH = oldPath;
+				oldPath = null;
+			}
 		});
-
-	return ret;
 };
